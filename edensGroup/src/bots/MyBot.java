7a83685667b39dd21c63c1541e853dd233ee994a
yuval <yuval@yuval-Inspiron-5559>
2019-02-18 15:13:43 +0200

    #4 זה מה שהלך להרצה 2

--- edensGroup/src/bots/MyBot.java
+++ edensGroup/src/bots/MyBot.java
@@ -1,33 +1,67 @@
 package bots;
 import elf_kingdom.*;
+
 import java.util.*;
 
 public class MyBot implements SkillzBot {
+	
+	public static final int MAX_MANA_FOR_ATTACK = 300;
+	public static int HALF_OF_ALACHSON;
+	
 	static Elf bunkerBuilderElf;
+	static ArrayList<Elf> allMyInitialElfForBunkering; // ask yuval
 	static ArrayList<Location> bunkerManaFLoocation = null, bunkerPortalsLocation = null;
-	static boolean bunkerFlag;
+	static boolean buildingBunkerFlag, GlobalBunkerFlagForAttacking;
 	
 	@Override
 	public void doTurn(Game game){
 		// TODO Auto-generated method stub
 		if(game.turn == 1) setup(game);
 		
-		bunkerFlag = buildBunker(game, bunkerBuilderElf);
-		if (bunkerFlag) bunker(game);
+		if(GlobalBunkerFlagForAttacking) {
+		// the bunker stuff
+		buildingBunkerFlag = buildBunker(game, bunkerBuilderElf);
+		bunker(game, buildingBunkerFlag);
+		
+		
+		boolean attackCondition = false; // just for have attack condition
+		if((game.getMyManaFountains().length >= 3 && ((game.getAllEnemyElves().length > game.getEnemyLivingElves().length)|| game.getMyMana() >= MAX_MANA_FOR_ATTACK)) || game.turn == 300) {
+			GlobalBunkerFlagForAttacking = false;
+			System.out.println("Let's attack");
+		}
+		
+		}else {
+			// the Attacking mitparetzet stuff
+			AttackingMode(game);
+			
+		}
 		
 		
 	}
 	
 	public static void setup(Game game) {
-		bunkerBuilderElf = game.getMyLivingElves()[0];
+		GlobalBunkerFlagForAttacking = true;
+		
+		/**
+		 * Elves #defining
+		 */
+		allMyInitialElfForBunkering = SkillzLib.convertArray2List(game.getMyLivingElves());
+		bunkerBuilderElf = allMyInitialElfForBunkering.remove(0); 
+		
 		/**
 		 * bunker global variables;
 		 */
-		bunkerManaFLoocation = SkillzLib.getMyBunkerMFLocs(game);
-		bunkerPortalsLocation = SkillzLib.getMyBunkerPLocs(game);
+		bunkerManaFLoocation = SkillzLib.getMyMFLocs(game);
+		bunkerManaFLoocation = SkillzLib.fllipArrayList(bunkerManaFLoocation);
+		System.out.println(bunkerManaFLoocation);
+		
+		bunkerPortalsLocation = SkillzLib.getMyPortalLocs(game);
+		bunkerPortalsLocation = SkillzLib.fllipArrayList(bunkerPortalsLocation);
+		System.out.println(bunkerPortalsLocation);
 	}
 	
 	public static boolean buildBunker(Game game, Elf builderElf) {
+		if(builderElf.isAlive()) {
 		if(! bunkerManaFLoocation.isEmpty()) {
 			//if the main queue is still empty - the MF are not finished
 			if(bunkerManaFLoocation.size() != bunkerPortalsLocation.size()) {
@@ -68,12 +102,62 @@ public class MyBot implements SkillzBot {
 			return false;
 		}
 		else return true;
+		}
+		else
+			return false;
 	}
 
-	public static boolean bunker(Game game) {
-		return true;
+	public static void bunker(Game game, boolean buildingFlag) {
+		if(buildingFlag == false) {
+			for(Elf myUnBuilderElves : allMyInitialElfForBunkering) {
+				SkillzLib.elfScout(game, myUnBuilderElves);
+			}
+			for(Portal defencePortal : game.getMyPortals()) {
+				SkillzLib.defendBunker(game, defencePortal);
+			}
+		}else {
+			for (Elf elf : game.getMyLivingElves()) {
+				SkillzLib.elfScout(game, elf);
+			}
+			for(Portal portal : game.getMyPortals()) {
+				SkillzLib.defendBunker(game, portal);
+			}
+		}
 	}
 	
 	
-
+	public static void AttackingMode(Game game) {
+		
+		ArrayList<Portal> myDefPortals = SkillzLib.getPortalsInRange(SkillzLib.convertArray2List(game.getMyPortals()), game.getMyCastle().getLocation(), SkillzLib.getHalfOfAlachson(game));
+		for(Portal p: myDefPortals) {
+			SkillzLib.defendBunker(game, p);
+		}
+		
+		for(Elf elf : game.getMyLivingElves()) {
+			SkillzLib.allOutMode(game, elf);
+		}
+		
+		ArrayList<Portal> myAttackPortals = SkillzLib.getPortalsInRange(SkillzLib.convertArray2List(game.getMyPortals()), game.getEnemyCastle().getLocation(), SkillzLib.getHalfOfAlachson(game));
+		/**
+		 * handle attack portals
+		 */
+		handleAttackPortals(myAttackPortals);
+	}
+	
+	
+	public static void handleAttackPortals(ArrayList<Portal> attackPortals) {
+		for (Portal portal : attackPortals) {
+			handleAttackPortal(portal);
+		}
+	}
+	
+	/**
+	 * need implementation
+	 * @param p
+	 */
+	public static void handleAttackPortal(Portal p) {
+		if(p.canSummonLavaGiant())
+			p.summonLavaGiant();
+	}
+	
 }
--- edensGroup/src/bots/SkillzLib.java
+++ edensGroup/src/bots/SkillzLib.java
@@ -1,6 +1,5 @@
 package bots;
-import java.util.ArrayList;
-
+import java.util.*;
 import elf_kingdom.*;
 
 public class SkillzLib {
@@ -23,7 +22,7 @@ public class SkillzLib {
 	}
 	
 	
-	public int howManyObjectsAroundLocation(Game game, Location location, GameObject[] objects, int range) {
+	public static int howManyObjectsAroundLocation(Game game, Location location, GameObject[] objects, int range) {
     	int count=0;
     	for(GameObject object: objects) {
     		if(object.inRange(location, range))
@@ -31,7 +30,7 @@ public class SkillzLib {
     	}
     	return count;
     }
-    public Portal returnClosestPortal(Location l, Portal[] stock) {
+    public static Portal returnClosestPortal(Location l, Portal[] stock) {
         int minDist = Integer.MAX_VALUE;
         Portal minPortal = null;
         for(Portal p : stock) {
@@ -43,7 +42,7 @@ public class SkillzLib {
         return minPortal;
     }
 
-    public Portal getClosestPortal(Game game, Castle enemyCastle){
+    public static Portal getClosestPortal(Game game, Castle enemyCastle){
 		Portal[] portals = game.getMyPortals();
 		Portal closest = null;
 		if(portals.length > 0){
@@ -62,53 +61,259 @@ public class SkillzLib {
 		return closest;
 	}
     
-    public static ArrayList<Location> getMyBunkerMFLocs(Game game){
-        int radius = game.castleSize+ game.portalSize;
-        Location middle = new Location(game.rows/2, game.cols/2);
-        Location myCastle = new Location(game.getMyCastle().location.row, game.getMyCastle().location.col);
-        int deltaR = myCastle.row - middle.row;
-        int deltaC = myCastle.col - middle.col;
-        double theta;
-        if(deltaC == 0)
-            theta = Math.PI/2;
-        else
-            theta = Math.atan(deltaR/deltaC);
-
-        if(deltaR < 0)
-            theta += Math.PI;
-
-        ArrayList<Location> locs = new ArrayList<Location>();
-        int row1, col1;
-        for(int i = 0; i < 3; i++) {
-            row1 = (int) (radius * Math.sin(theta - (i-1) * Math.PI/4)) + 1;
-            col1 = (int) (radius * Math.cos(theta - (i-1) * Math.PI/4)) + 1;
-            locs.add(new Location(row1, col1));
+    
+    public static GameObject getClosestEnemy(GameObject obj, Game game){
+        //can return null! be careful
+        GameObject closest = null;
+        int minimumDistance = game.cols;//max distance for which we care for
+        if(game.getEnemyCreatures().length != 0){
+        // if everything is OK
+        closest = game.getEnemyCreatures()[0];//if there are no creatures, stays a null
+        for(int i = 0; i<game.getEnemyCreatures().length; i++){
+            if(obj.distance(game.getEnemyCreatures()[i]) <= minimumDistance){
+                closest = game.getEnemyCreatures()[i];
+                minimumDistance = obj.distance(closest);
+            }
         }
-        return locs;
+        }
+        
+        //checks for all living elves, if there are none might give null
+        for(Elf e: game.getEnemyLivingElves()){
+            if(obj.distance(e) < minimumDistance){
+                closest = e;
+                minimumDistance = obj.distance(closest);
+            }
+        }
+
+        /*returns null when there are no enemies who are distant 
+        less than the screen's width
+        */
+        return closest;
+
     }
     
-    public static ArrayList<Location> getMyBunkerPLocs(Game game) {
+    
+   
+    public static ArrayList<Location> getMyMFLocs(Game game){
+		int radius = game.castleSize+ game.portalSize +1;
+		Location middle = new Location(game.rows/2, game.cols/2);
+		Location myCastle = new Location(game.getMyCastle().location.row, game.getMyCastle().location.col);
+		double deltaR = myCastle.row - middle.row;
+		double deltaC = myCastle.col - middle.col;
+		double theta;
+		System.out.println(deltaR);
+		System.out.println(deltaC);
+		if(deltaC == 0)
+			theta = Math.PI/2;
+		else
+			theta = Math.atan(deltaR/deltaC);
 		
-    	return null;
+		System.out.println(theta);
+		if(deltaC > 0)
+			theta += Math.PI;
+		System.out.println(theta);
+		
+		System.out.print(radius + game.portalSize);
+		ArrayList<Location> locs = new ArrayList<Location>();
+		int row1, col1;
+		for(int i = 0; i < 3; i++) {
+		    if(i == 1){
+		        row1 = (int) ((1.1*radius) * Math.sin(theta - (i-1) * Math.PI/5)) + 1;
+			    col1 = (int) ((1.1*radius) * Math.cos(theta - (i-1) * Math.PI/5)) + 1;
+			    System.out.println(col1);
+			    System.out.println(myCastle.col);
+		    }
+		    else{
+			    row1 = (int) ((radius + game.portalSize) * Math.sin(theta - (i-1) * Math.PI/5)) + 1;
+			    col1 = (int) ((radius + game.portalSize) * Math.cos(theta - (i-1) * Math.PI/5)) + 1;
+		    }
+			locs.add(new Location(myCastle.row + row1, myCastle.col + col1));
+		}	
+		return locs;
+	}
+	
+	public static ArrayList<Location> getMyPortalLocs(Game game){
+		Location l1, l2;
+		int radius = game.castleSize+ game.portalSize+1;
+		Location middle = new Location(game.rows/2, game.cols/2);
+		Location myCastle = new Location(game.getMyCastle().location.row, game.getMyCastle().location.col);
+		int deltaR = myCastle.row - middle.row;
+		int deltaC = myCastle.col - middle.col;
+		double theta;
+		if(deltaC == 0)
+			theta = Math.PI/2;
+		else
+			theta = Math.atan(deltaR/deltaC);
+		
+		if(deltaC > 0)
+			theta += Math.PI;
+		Portal p = new Portal();
+		ArrayList<Location> locs = new ArrayList<Location>();
+		l1 = new Location(myCastle.row + (int) ((1.9*radius)* Math.sin(theta - Math.PI/8) + 1), myCastle.col + (int) ((1.9*radius)* Math.cos(theta + Math.PI/8) + 1));
+		l2 = new Location(myCastle.row + (int) ((1.9*radius)* Math.sin(theta - Math.PI/3) + 1), myCastle.col + (int) ((1.9*radius)* Math.cos(theta - Math.PI/8) + 1));
+		locs.add(l1);
+		locs.add(l2);
+		return locs;
+		
 	}
     
     
-    public static boolean isBunkerInDanger(Game game, Portal[]portals) {
-        for(Portal p:portals) {
-            for(Elf e:game.getEnemyLivingElves()) {
-                if(p.inRange(e, 4*(e.attackRange)))
-                    return true;
-            }
-            for(Creature c:game.getEnemyCreatures()) {
-                if(p.inRange(c, (c.attackRange)*4)) {
-                    return true;
-                }
+	
+	public static<T> ArrayList<T> fllipArrayList(ArrayList<T> array){
+		ArrayList<T> cop = new ArrayList<>();
+		
+		for(T t : array) {
+			cop.add(t);
+		}
+		
+		ArrayList<T> arrayL = new ArrayList<>();
+		while(!cop.isEmpty()) {
+			arrayL.add(cop.remove(cop.size()-1));
+		}
+		
+		return arrayL;
+	}
+	
+	
+	public static void elfScout(Game game, Elf elf) {
+		if(elf.isAlive()) {
+			if(isInDanger(elf,game)) {
+				if(SkillzLib.returnClosestPortal(elf.location, game.getMyPortals())==null)
+					elf.moveTo(game.getMyCastle());
+				else
+					elf.moveTo(SkillzLib.returnClosestPortal(elf.location, game.getMyPortals()));
+			}else {
+				Building min = null;
+				Location l1 = new Location(0,0);
+				Location l2 = new Location(game.rows-1,game.cols-1);
+				int minDistance = l1.distance(l2);
+				for(Building b:game.getEnemyPortals()) { 
+					if(minDistance>elf.distance(b)) {
+						min=b;
+						minDistance=elf.distance(b);
+					}
+				}
+				for(Building b:game.getEnemyManaFountains()) {
+					if(minDistance>elf.distance(b)) {
+						min=b;
+						minDistance=elf.distance(b);
+					}
+				}
+				if(min==null) {
+					elf.moveTo(game.getMyCastle());
+				}else {
+					if(elf.inAttackRange(min))
+						elf.attack(min);
+					else
+						elf.moveTo(min);
+				}
+			}
+		}
+	}
+
+	
+	public static boolean isInDanger(Elf e, Game g){
+        for (Elf elf : g.getEnemyLivingElves()){
+            if (elf.inRange(e, 2*elf.attackRange))
+                return true;
+        }
+        for(IceTroll iceTroll : g.getEnemyIceTrolls()){
+            if (iceTroll.inRange(e, 2*iceTroll.attackRange)){
+                return true;
             }
         }
         return false;
     }
 	
 	
+	public static boolean defendBunker(Game game, Portal p){
+        //gets portal p for check
+        /*this function returns false if summoning an ice troll is not necessary 
+        or impossible, returns true if executed correctly and summoned ice troll*/
+        GameObject closest = getClosestEnemy(p, game);
+        if(closest == null){
+            //no enemies, or enemies further from screen's width distance
+            return false;
+        }
+
+        int life = 0;
+        int maxLife = 0;
+        if(closest instanceof Elf){
+            life = ((Elf) closest).currentHealth;
+            maxLife = ((Elf) closest).maxHealth;
+        }else if(closest instanceof LavaGiant){
+            life = ((LavaGiant) closest).currentHealth;
+            maxLife = ((LavaGiant) closest).maxHealth;
+        }else{
+            life = ((IceTroll) closest).currentHealth;
+            maxLife = ((IceTroll) closest).maxHealth;
+        }
+
+        if(closest.distance(p) < 4*game.elfAttackRange && life > maxLife / 3){
+            if(p.canSummonIceTroll()){
+                p.summonIceTroll();
+            }else{
+                //could'nt summon ice troll for defense, return false
+                return false;
+            }
+        }
+
+        //executed correctly, summoned ice troll 
+        return true;
+
+    }
 	
+	public static void allOutMode(Game game, Elf elf){
+        Castle enemyCastle = game.getEnemyCastle();
+        int distanceFromCenter = new Location(game.cols/2, game.rows/2).distance(enemyCastle);
+        if(elf.distance(enemyCastle) <= 0.5*distanceFromCenter && elf.canBuildPortal()){
+            elf.buildPortal();
+            return;
+        }
+        if(elf.distance(enemyCastle) <= distanceFromCenter){
+            if(!elf.canCastSpeedUp() && game.getMyMana() > 110){
+                elf.castSpeedUp();
+                return;
+            }
+            if(!elf.invisible && elf.canCastInvisibility()){
+                elf.castInvisibility();
+                return;
+            }
+
+        }
+        if(elf.inAttackRange(enemyCastle)){
+            elf.attack(enemyCastle);
+            return;
+        }
+        elf.moveTo(enemyCastle);
+
+    }
+	
+	
+	public static<T> ArrayList<T> convertArray2List(T[] array){
+		ArrayList<T> ret = new  ArrayList<>();
+		for(T t : array) {
+			ret.add(t);
+		}
+		return ret;
+	}
+	
+	
+	public static ArrayList<Portal> getPortalsInRange(ArrayList<Portal> stock, Location loc, int range){
+		ArrayList<Portal> newArray = new ArrayList<>();
+		for(Portal p : stock) {
+			if(p.inRange(loc, range)) {
+				newArray.add(p);
+			}
+		}
+		
+		return newArray;
+	}
+	
+	public static int getHalfOfAlachson(Game game) {
+		int need2BeShores = (game.cols*game.cols) + (game.rows*game.rows);
+		int alacson = (int) Math.sqrt(need2BeShores);
+		return alacson/2;
+	}
 	
 }
